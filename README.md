2c5da01a72e716d8143dcb2d66ee98ed84ec3a9b
GhsJahanmir <85613062+GhsJahanmir@users.noreply.github.com>
2025-10-03 18:44:42 +0800

    adding requirements, readme and setup file

---
+++ README.md
@@ -0,0 +1,6 @@
+# chatbot using Flask, Pinecone, LangChain and AzureOpenAI
+
+#create environment in the anaconda prompt
+conda create -n chatbot  python=3.10  -y 
+#activate it
+conda activate chatbot
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatbot_app",
+    version="0.1.0",
+    author="Navigator appbot",
+    author_email="Navigatorappbot@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
